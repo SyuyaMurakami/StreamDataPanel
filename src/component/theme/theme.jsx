ceba91d0f9445d2b27c03ef8a5d27e8e7cf6b7e0
SyuyaMurakami <89246518+SyuyaMurakami@users.noreply.github.com>
2025-10-21 22:23:23 +0800

    add theme

--- src/component/theme/theme.jsx
+++ src/component/theme/theme.jsx
@@ -23,6 +23,41 @@ const allThemes = {
     '--text-color': '#eeeeee',
     '--primary-color-hover': 'rgba(33, 150, 243, 0.8)',
   },
+  coffee: {
+    '--primary-color': '#5D4037',
+    '--secondary-color': '#A1887F',
+    '--background-color': '#4E342E',
+    '--text-color': '#FBEFF4',
+    '--primary-color-hover': 'rgba(121, 85, 72, 0.8)',
+  },
+  night: {
+    '--primary-color': '#37474F',
+    '--secondary-color': '#90A4AE',
+    '--background-color': '#263238',
+    '--text-color': '#CFD8DC',
+    '--primary-color-hover': 'rgba(84, 110, 122, 0.8)',
+  },
+  moss: {
+    '--primary-color': '#556B2F',
+    '--secondary-color': '#A4B48A',
+    '--background-color': '#495638',
+    '--text-color': '#E8F5E9',
+    '--primary-color-hover': 'rgba(55, 71, 79, 0.8)',
+  },
+  sunset: {
+    '--primary-color': '#BF360C',
+    '--secondary-color': '#FFAB91',
+    '--background-color': '#A61C00',
+    '--text-color': '#FBE9E7',
+    '--primary-color-hover': 'rgba(216, 67, 21, 0.8)',
+  },
+  lake: {
+    '--primary-color': '#006064',
+    '--secondary-color': '#80CBC4',
+    '--background-color': '#004D40',
+    '--text-color': '#E0F7FA',
+    '--primary-color-hover': 'rgba(0, 131, 143, 0.8)',
+  },
 };
 
 const ThemeContext = createContext();
